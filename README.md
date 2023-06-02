fe3bccee64d7650178516b030278e1bcc97a7250
Martin Soemer <martin.soemer@bsi-software.com>
2023-06-02 11:02:07 +0200

    Initial Release

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Latest Version](https://img.shields.io/github/v/release/soemer/invisible-tokens-can-see-too?display_name=tag&sort=semver&label=Latest%20Version)](https://github.com/soemer/invisible-tokens-can-see-too/releases/latest)
+![Foundry Version](https://img.shields.io/endpoint?url=https://foundryshields.com/version?url=https%3A%2F%2Fraw.githubusercontent.com%2Fsoemer%2Finvisible-tokens-can-see-too%2Fmain%2Fmodule.json)
+
+# Invisible tokens can see too (Foundry VTT Module)
+
+A module that allows tokens to be invisible and keeping their sight.
+This allows a player to be invisible to the rest of group and sneak around without the other players knowing the position. 
---
+++ scripts/invisibleTokensCanSeeToo.js
@@ -0,0 +1,66 @@
+const MODULE_NAME = 'InvisibleTokensCanSeeToo';
+const MODULE_ID = 'invisible-tokens-can-see-too';
+const PROPERTY_MODULE_ENABLED = 'invisibleTokensCanSee';
+
+Hooks.once('setup', ()=>{
+	console.info(MODULE_NAME + ' | Initializing module');
+	registerDependencyChecks();
+	registerSettings();
+	registerVisionSourceCalculation();
+});
+
+function registerSettings() {
+    game.settings.register(MODULE_NAME, PROPERTY_MODULE_ENABLED, {
+		name: game.i18n.localize(MODULE_ID + '.Settings.InvisibleTokensCanSee.Name'),
+		hint: game.i18n.localize(MODULE_ID + '.Settings.InvisibleTokensCanSee.Hint'),	
+		scope: 'world',
+		config: true,
+		type: Boolean,
+		default: true
+    });
+}
+
+function registerDependencyChecks() {
+	Hooks.once("canvasInit", () => {
+        if (!game.modules.get("lib-wrapper")?.active) {
+            Notifications.error('libWrapper is required!', { permanent: true, console: false });
+			Console.error("libWrapper is not enabled");
+		}
+	});
+}
+
+function registerVisionSourceCalculation() {
+	libWrapper.register(
+		MODULE_ID,
+		'Token.prototype._isVisionSource',
+		function (wrapped) {
+			if (wrapped()) {
+				return true;
+			}
+			
+			return isVisionModified(this);
+		},
+		'WRAPPER');
+		
+	libWrapper.register(
+		MODULE_ID,
+		'Token.prototype.isVisible', 
+		function(wrapped) {
+			if (wrapped()) {
+				return true;
+			}
+			
+			return isVisionModified(this);
+		},
+		'WRAPPER');
+}
+
+function isVisionModified(token) {
+	let moduleEnabled = game.settings.get(MODULE_NAME, PROPERTY_MODULE_ENABLED);
+			
+	if (moduleEnabled) {
+		return token.hasSight && token.observer;
+	}
+		
+	return false;
+}
